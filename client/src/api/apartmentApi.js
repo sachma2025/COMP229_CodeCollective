b21c991d1176e7730cd4c36a1363bbac23d5f983
ifejika <83151638+ifejika@users.noreply.github.com>
2024-12-05 17:17:44 -0500

    client

--- client/src/api/apartmentApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const API_URL = 'http://localhost:3000/api';
-
-export const getApartments = () => axios.get(`${API_URL}/apartments`);
-export const createApartment = (apartment) => axios.post(`${API_URL}/apartments`, apartment);
-export const updateApartment = (id, apartment) => axios.put(`${API_URL}/apartments/${id}`, apartment);
-export const deleteApartment = (id) => axios.delete(`${API_URL}/apartments/${id}`);
--- server/routes/apartments.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createNewApartment, getAllApartments } from '../controllers/apartments.controller.js';
-
-const router = express.Router();
-
-router.get('/', getAllApartments);
-router.post('/', createNewApartment);
-
-export default router;
