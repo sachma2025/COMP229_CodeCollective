da16d5ade556a93ba9589319d196d6098990a8e5
Sachin Mapa <smapa@my.centennialcollege.ca>
2024-12-05 04:14:20 -0600

    Added update and delete functionality for apartments

--- server/controllers/apartments.controller.js
+++ server/controllers/apartments.controller.js
@@ -19,6 +19,28 @@ export const getAllApartments = async (req, res) => {
   }
 }
 
+export const deleteApartment = async (req, res) => {
+  try {
+    const deletedApartment = await Apartment.findByIdAndDelete(req.params.id);
+    res.status(200).json({deletedApartment});
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete apartment' });
+  }
+};
+
+export const updateApartment = async (req, res) => {
+  try {
+    const updatedApartment = await Apartment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedApartment);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to update apartment' });
+  }
+};
+
 /*export const getAllApartments = async (req, res) => {
   const apartments = await Apartment.find()
   res.json(apartments)
--- server/routes/apartments.routes.js
+++ server/routes/apartments.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createNewApartment, getAllApartments } from '../controllers/apartments.controller.js';
+import { createNewApartment, getAllApartments, deleteApartment, updateApartment } from '../controllers/apartments.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllApartments);
 router.post('/', createNewApartment);
+router.delete('/:id', deleteApartment);
+router.put('/:id', updateApartment);
 
 export default router;
